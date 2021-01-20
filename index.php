d5e60ea3cea025c7f4ac843c0673d378a37731c3
swaragjatla7 <74284486+swaragjatla7@users.noreply.github.com>
2021-01-20 13:09:57 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Fontchanger
+ * Plugin URI: https://wordpress.org/plugins/Fontchanger/
+ * Description: customize the theme fonts and colors that you use on your website.
+ * Version: 1.0
+ * Author: Surya Sri Swarag Jatla
+ * @package Fontchanger
+ */
+
+
+require_once 'includes/fonts.php';
+
+require_once 'includes/class.Fontchanger.php';
+
+require_once 'includes/helpers.php';
